304d933fd3c1f4ad6b4e0dac48b474186416f749
Beknasar <680633@gmail.com>
2023-03-15 14:35:07 +0600

    Fixed urls for static files in header and footer.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return render_template("index.html")
+
+    return render_template("index.html", )
+
 
 
 if __name__ == "__main__":
