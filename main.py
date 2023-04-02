9faf31b30d180b3c9b74fe74a717d6b32ae31466
Beknasar <680633@gmail.com>
2023-04-02 23:13:50 +0300

    Created contact page, added url and function.

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ def index():
     return render_template("index.html", )
 
 
-@app.route('/about')
+@app.route('/about_page')
 def about_page():
     return render_template('about.html')
+
+
+@app.route('/contact_page')
+def contact_page():
+    return render_template('contact.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
