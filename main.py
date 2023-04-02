65a57323cccc629bcf9b3c170a9831b0cd83b294
Beknasar <680633@gmail.com>
2023-04-02 20:41:50 +0300

    Created header and footer template instead of long templates paths. Created requirements.txt

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
-
     return render_template("index.html", )
-
 
 
 if __name__ == "__main__":
