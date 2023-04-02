36040ada84b10247a8b9bff41d08c0addc456696
Beknasar <680633@gmail.com>
2023-04-03 01:20:59 +0300

    Added post_page, urls and function. Changed post urls in index.html file.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
-
+import requests
 app = Flask(__name__)
+
+
+response = requests.get("https://api.npoint.io/6a763fd44db522817a35")
+all_posts = response.json()
 
 @app.route('/')
 def index():
-    return render_template("index.html", )
+    return render_template("index.html", posts=all_posts)
 
 
 @app.route('/about_page')
@@ -15,6 +19,14 @@ def about_page():
 @app.route('/contact_page')
 def contact_page():
     return render_template('contact.html')
+
+@app.route("/post/<int:id>")
+def post_page(id):
+    for blog_post in all_posts:
+        if blog_post['id'] == id:
+            post = blog_post
+
+    return render_template("post.html", post=post)
 
 
 if __name__ == "__main__":
