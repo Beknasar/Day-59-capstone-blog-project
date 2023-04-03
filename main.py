9f76070dde5d1d2ec5e44ab7976e10fca43958b1
Beknasar <680633@gmail.com>
2023-04-03 04:17:10 +0300

    Added email sending with smtplib. Updated the contact.html file so that the <h1> on the contact.html file becomes "Successfully sent message".

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, request
+import requests, smtplib
 app = Flask(__name__)
 
 
 response = requests.get("https://api.npoint.io/6a763fd44db522817a35")
 all_posts = response.json()
+
+my_email = "beknazar.ulanbekuuluu@mail.ru"
+password = "DGjDj2QjrUDdxnGDabg3"
 
 @app.route('/')
 def index():
@@ -16,17 +19,32 @@ def about_page():
     return render_template('about.html')
 
 
-@app.route('/contact_page')
+@app.route('/contact_page', methods=["GET", "POST"])
 def contact_page():
+    if request.method == "POST":
+        data = request.form
+        for key, value in request.form.items():
+            print(f"{key}: {value}")
+
+        with smtplib.SMTP("smtp.mail.ru") as connection:
+            # secure our connection to email server
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=data['email'],
+                msg=f"Subject:Conversation\n\n {data['message']}"
+            )
+
+        return render_template('contact.html', data=data)
     return render_template('contact.html')
 
 @app.route("/post/<int:id>")
 def post_page(id):
     for blog_post in all_posts:
         if blog_post['id'] == id:
-            post = blog_post
-
-    return render_template("post.html", post=post)
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
